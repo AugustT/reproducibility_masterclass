0de167474506f9210a06cd0d56381809621b1163
Tom August <tomaug@ceh.ac.uk>
2020-11-14 17:33:00 +0000

    update setup from use_course()

--- setup.R
+++ setup.R
@@ -1,3 +1,3 @@
 # Code for getting the course materials set up
 install.packages('usethis')
-usethis::use_course('AugustT/reproducibility_masterclass')
+usethis::use_course('https://github.com/AugustT/reproducibility_masterclass/raw/master/master.zip')
